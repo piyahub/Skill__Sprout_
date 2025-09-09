546567f405e2c63669d00e8629f0857200d4220f
Priya Saxena <76806897+piyahub@users.noreply.github.com>
2025-09-09 19:38:24 +0530

    Update App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Login from './Components/Login';
-import Contact from './Components/Contact';
+// import Contact from './Components/Contact';
 import Register from './Components/Register';
 import Dashboard from './Components/Dashboard';
 import InterviewPrep from './Components/InterviewPrep';
 import CareerPath from './Components/Carrerpath';
-import EmailGenerator from './Components/EmailGenerator';
-import ResumeEmbed from './Components/ResumeEmbed';
+// import EmailGenerator from './Components/EmailGenerator';
+// import ResumeEmbed from './Components/ResumeEmbed';
 import Footer from './Components/Footer';
 import JobOpportunitiesAnalyzer from './Components/JobOpportunitiesAnalyzer';
-import LinkedIn from './Components/LinkedIn';
-import SkillAnalyzer from './Components/SkillAnalyzer';
+// import LinkedIn from './Components/LinkedIn';
+// import SkillAnalyzer from './Components/SkillAnalyzer';
 import About from './Components/About';
 import Assesment from './Components/Assesment';
 
@@ -41,17 +41,17 @@ const App = () => {
         <Route path="/" element={<LayoutWithFooter><Home /></LayoutWithFooter>} />
         <Route path="/login" element={<LayoutWithFooter><Login /></LayoutWithFooter>} />
         <Route path="/register" element={<LayoutWithFooter><Register /></LayoutWithFooter>} />
-        <Route path="/contact" element={<LayoutWithFooter><Contact /></LayoutWithFooter>} />
-        <Route path="/skillanalyzer" element={<LayoutWithFooter><SkillAnalyzer /></LayoutWithFooter>} />
+{/*         <Route path="/contact" element={<LayoutWithFooter><Contact /></LayoutWithFooter>} /> */}
+{/*         <Route path="/skillanalyzer" element={<LayoutWithFooter><SkillAnalyzer /></LayoutWithFooter>} /> */}
         <Route path="/carreranalyzer" element={<LayoutWithFooter><JobOpportunitiesAnalyzer /></LayoutWithFooter>} />
         <Route path="/dashboard" element={<LayoutWithFooter><Dashboard /></LayoutWithFooter>} />
 
         {/* Routes without Footer */}
         <Route path="/interview-prep" element={<LayoutWithoutFooter><InterviewPrep /></LayoutWithoutFooter>} />
         <Route path="/career" element={<LayoutWithoutFooter><CareerPath /></LayoutWithoutFooter>} />
-        <Route path="/email" element={<LayoutWithoutFooter><EmailGenerator /></LayoutWithoutFooter>} />
-        <Route path="/resume" element={<LayoutWithoutFooter><ResumeEmbed /></LayoutWithoutFooter>} />
-        <Route path="/linkedin" element={<LayoutWithoutFooter><LinkedIn /></LayoutWithoutFooter>} />
+{/*         <Route path="/email" element={<LayoutWithoutFooter><EmailGenerator /></LayoutWithoutFooter>} /> */}
+{/*         <Route path="/resume" element={<LayoutWithoutFooter><ResumeEmbed /></LayoutWithoutFooter>} /> */}
+{/*         <Route path="/linkedin" element={<LayoutWithoutFooter><LinkedIn /></LayoutWithoutFooter>} /> */}
         <Route path="/about" element={<LayoutWithoutFooter><About /></LayoutWithoutFooter>} />
         <Route path="/assesment" element={<LayoutWithoutFooter><Assesment /></LayoutWithoutFooter>} />
       </Routes>
